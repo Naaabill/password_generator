7db2b27d3b700decf697538dabb5b9a54895515c
Nabil <95193425+Naaabill@users.noreply.github.com>
2024-01-02 23:27:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,20 @@ const copyContent = async () => {
     }
 }
 
-
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("text");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
 
 console.log(password_length)
 display()
